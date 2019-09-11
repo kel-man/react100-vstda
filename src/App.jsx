df6f9f4b000ac52d1579aa1735adf9fbf0c535f0
Kelen Y. Yafuso <kyyafuso@gmail.com>
2019-09-11 16:37:24 -0700

    Still building framework. Multiple potential approaches are commented out for now

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,121 @@
 import React, { Component } from 'react';
+import ListItem from './listitem';
+import axios from 'axios';
+/**It is imperative to plan this out correctly. Everything about the structure must be premeditated
+ * DO NOT RUSH
+ * DO NOT PANIC
+ * DO NOT GET FRUSTRATED
+ * JUST GO PIECE BY PIECE
+ * YOU GOT THIS 
+ * Create an axios structure for writing and pulling each item from JSON file for ToDos
+ * the rest of the structure for this program is just a combination of TopSpots and Change Calculator, with a ListItem component fit within a couple containers using the json information to populate each element
+ */
+
+//  const TodoForm = ({addTodo}) => {
+//    let input;
+//    return (
+//      <div>
+//        input ref={node => {
+//          input = node;
+//       }}/>
+//       <button onClick={() => {
+//         addTodo(input.value);
+//         input.value = '';
+//       }}>
+//         +
+//       </button>
+//      </div>
+//    );
+//  };
+
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      priority:'',
+      length:'',
+      name:'',
+      details:'',
+      editable:'',
+      removable:''
+    }
+    this.handleSelect=this.handleSelect.bind(this);
+    // this.handleSubmit=this.handleSubmit.bind(this);
+    // this.handleNameChange=this.handleNameChange.bind(this);
+    // this.handleDescriptionChange=this.handleDescriptionChange.bind(this); 
+  }
 
+  // compontentWillMount(){
+  //   axios
+  //   .get('filenamegoeshere.json')
+  //   .then(response => list.data)
+  //   .then()
+  // }
+  
+  // getInitialState: function(){
+  //   return{
+  //     'city': '',
+  //     'zip': '',
+  //     'temp': ''
+  //   }
+  // }
+
+  handleSelect(e){
+    this.setState({
+      priority: e.target.value
+    });
+  }
+  
+  handleNameChange(e){
+    this.setState({
+      name: e.target.value
+    })
+  }
+  
+  handleDescriptionChange(e){
+    this.setState({
+      description: e.target.value
+    })
+  }
+  
+  /* handleSubmit(e){
+      let newItem = {
+        priority: this.state.priority,
+        name: this.state.name,
+        details: this.state.details,
+        editable: this.state.editable,
+        removable: this.state.removable
+      }
+      this.setState({
+        items: newItem
+      });
+  }*/
+  \
   render() {
     return (
-      <div className='container'>
-        
-      </div>
+      
+       <div className='container' style={{display:'flex', flexFlow:'column wrap', justifyContent:'space-around', alignContent:'space-around', height:'100%'}}>
+         <ListItem/>
+         <ListItem/>
+         <ListItem/>
+      {/* //   <h1 style={{color:'black', padding:'20px'}}>Very Simple To-Do Application</h1>
+         <hr style={{borderTop:'3px solid white'}}/>
+         <form className='input form card'>
+          <div className='input card' style={{display:'flex', flexFlow:'row wrap', alignSelf:'space-around', alignContent:'space-around', height:'100%'}}>
+            <div className='inputBox' style={{width:'30%', justifyContent:'left'}}>
+              <h3 className='box-title' style={{color:'black', backgroundColor:'#f5f5f5', alignContent:'left'}}>Add New Todo: I want to...</h3>
+              <input className='create-todo-text card'/>
+              <select className='priority card' onChange={this.handleSelect} type='number' value={this.state.priority}>
+                <option className='low-priority card' value='1' style={{}}>Low</option>
+                <option className='mid-priority card' value='2' style={{}}>Medium</option>
+                <option className='high-priority card' value='3' style={{}}>Critical!!!</option>
+              </select>
+              <button type='submit' className='submit' onClick={this.handleSubmit} />
+            </div>
+         </div> 
+        </form>*/}
+       </div>
     );
   }
 }
---
+++ src/listitem.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+// class ListItem extends Component {
+  // render(){
+    // return(
+     export default props => (
+      <div style={{}}>
+        <p className='item-number'>Test Item # Here(333){/*this.state.number*/}</p>
+        <h2 className='item-description card' style={{/*color:this.state.prior*/}}>Test Description Here{props.description}</h2>
+        <button className='edit button btn-primary btn'>Edit Placeholder</button>
+        <button className='button remote btn'>Delete Placeholder</button>
+      </div>
+    );
+//   }
+// }
